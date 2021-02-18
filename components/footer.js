010b7b4d92283791c4ecf478392eff8ac7ddb3aa
Modestas <m.kazinauskas@gmail.com>
2021-02-18 02:39:33 +0200

    Updates to view configuration

--- components/footer.js
+++ components/footer.js
@@ -5,18 +5,26 @@ function Footer({ contactUsLink }) {
     return (
         <footer class="flex justify-center px-4 text-gray-800 bg-white dark:text-white dark:bg-gray-800">
             <div class="container py-6">
-                <h1 class="text-lg font-bold text-center lg:text-2xl">
-                    Join 31,000+ other and never miss <br /> out on new tips, tutorials, and more.
-            </h1>
 
-                <div class="flex justify-center mt-6">
-                    <div class="bg-white border rounded-md focus-within:ring focus-within:border-blue-500">
-                        <div class="flex flex-wrap justify-between md:flex-row">
-                            <input type="email" class="p-2 m-1 text-sm text-gray-700 appearance-none focus:outline-none focus:placeholder-transparent" placeholder="Enter your email" aria-label="Enter your email" />
-                            <button class="w-full p-2 m-1 text-sm font-semibold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded lg:w-auto hover:bg-gray-700">subscribe</button>
+                <section class="flex flex-col max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 md:flex-row md:h-48">
+                    <div class="md:flex md:items-center md:justify-center md:w-1/2 md:bg-gray-700 md:dark:bg-gray-800">
+                        <div class="px-6 py-6 md:px-8 md:py-0">
+                            <h2 class="text-lg font-bold text-gray-700 dark:text-white md:text-gray-100">Sign Up For <span class="text-blue-600 dark:text-blue-400 md:text-blue-300">Project</span> Updates</h2>
+
+                            <p class="mt-2 text-sm text-gray-600 dark:text-gray-400 md:text-gray-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur obcaecati odio</p>
                         </div>
                     </div>
-                </div>
+
+                    <div class="flex items-center justify-center pb-6 md:py-0 md:w-1/2">
+                        <form>
+                            <div class="flex flex-col overflow-hidden border rounded-lg dark:border-gray-600 lg:flex-row">
+                                <input class="px-6 py-3 text-gray-700 placeholder-gray-500 bg-white outline-none dark:bg-gray-800 dark:placeholder-gray-400 focus:placeholder-transparent dark:focus:placeholder-transparent" type="text" name="email" placeholder="Enter your email" aria-label="Enter your email" />
+
+                                <button class="px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-200 transform bg-gray-700 hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">subscribe</button>
+                            </div>
+                        </form>
+                    </div>
+                </section>
 
                 <hr class="h-px mt-6 border-gray-300 border-none dark:bg-gray-700" />
 
