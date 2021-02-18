02a00b5ad96a3f76ea89a1d2d96f6d8f98a12f0e
Modestas <m.kazinauskas@gmail.com>
2021-02-19 00:14:54 +0200

    New screens and background

--- components/main/section-about-us.js
+++ components/main/section-about-us.js
@@ -8,7 +8,7 @@ function SectionAboutUs() {
 
                 <div class="px-6 py-12 lg:max-w-5xl lg:w-1/2 sm:w-full text-center">
                     <h2 class="text-2xl font-bold text-gray-800 dark:text-white md:text-3xl">Relax by listening online radio with us</h2>
-                    <p class="mt-4 text-gray-600 dark:text-gray-400">We are all used to listening to the radio through a good old receiver on either AM or FM frequencies, but now, every radio station offers digital broadcasting on the Internet. We live in unique times when free Internet radio stations from around the world are just one button away. Radio stations offer a variety of programs, news and music in a wide range of styles and trends. Therefore, to help you listen to the radio online for free we endeavoured to gather all the stations of the world in one place. Come to our site or install our free application on your smartphone, and we will do our best to make it convenient for you to listen to your favorite radio station.</p>
+                    <p class="mt-4 text-gray-600 dark:text-gray-400">We are all used to listening to the radio through a good old receiver on either AM or FM frequencies, but now, every radio station offers digital broadcasting on the Internet. We live in unique times when free Internet radio stations from around the world are just one button away. Radio stations offer a variety of programs, news and music in a wide range of styles and trends. Come to our site or install our free application on your smartphone, and we will do our best to make it convenient for you to listen to your favorite radio station.</p>
                 </div>
             </div>
         </section>
