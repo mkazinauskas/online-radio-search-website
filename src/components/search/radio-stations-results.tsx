561bcb385e8a280315f4ed3a42dc3c8672b96fec
Modestas <m.kazinauskas@gmail.com>
2021-02-27 22:13:27 +0200

    Navigation in search tabs

--- src/components/search/radio-stations-results.tsx
+++ src/components/search/radio-stations-results.tsx
@@ -1,10 +1,10 @@
 import NoSearchResultsComponent from "./no-search-results-component";
 import { Response, Data } from '../../api/search/radio-stations-search';
 import PaginationComponent from "./pagination-component";
+import SearchTypeTabsComponent, { ActiveTabType } from "./search-type-tabs-component";
 
-function RadioStationsResults(params: { searchResults: Response}) {
+function RadioStationsResults(params: { searchResults: Response }) {
     const { searchResults } = params;
-    console.log(searchResults);
 
     if (searchResults.success === false) {
         return (
@@ -18,9 +18,12 @@ function RadioStationsResults(params: { searchResults: Response}) {
 
     return (
         <section className="bg-gray-50">
-            <h1 className="text-2xl text-secondary font-bold flex-1 px-10 md:pt-16 py-5 text-center">Radio stations with name: "{searchResults.query}"</h1>
-            {radioStationResults}
-            <PaginationComponent baseUrl='/search/by-radio-station' query={searchResults.query} currentPage={searchResults.number} lastPage={searchResults.totalPages} />
+            <div className="container mx-auto">
+                <h1 className="text-2xl text-secondary font-bold flex-1 md:py-16 py-5 text-center">Radio stations with name: "{searchResults.query}"</h1>
+                <SearchTypeTabsComponent query={searchResults.query} activeTab={ActiveTabType.RADIO_STATIONS}/>
+                {radioStationResults}
+                <PaginationComponent baseUrl='/search/by-radio-station' query={searchResults.query} currentPage={searchResults.number} lastPage={searchResults.totalPages} />
+            </div>
         </section>
     )
 
@@ -32,7 +35,7 @@ function stationResult(item?: Data) {
     }
 
     return (<div key={item.id}>
-        <article className="sm:grid grid-cols-5 bg-white shadow-sm p-5 relative sm:p-2 rounded-lg lg:col-span-2 my-5 mx-5 md:mx-20 md:my-10">
+        <article className="sm:grid grid-cols-5 bg-white shadow-sm p-5 relative sm:p-2 rounded-lg lg:col-span-2 my-2 md:mb-10">
             <img src={item.logoUrl} alt={`Logo of ${item.title} radio station`} className="max-h-64 rounded-lg m-auto" />
             <div className="py-5 self-center sm:pt-0 sm:px-5 col-span-4">
                 <h2 className="text-gray-800 capitalize text-xl font-bold mb-5 text-center md:text-left">{item.title}</h2>
