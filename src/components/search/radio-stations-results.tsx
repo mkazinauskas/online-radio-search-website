a49a231d13ad48d496d874d68a244fd39430ddc4
Modestas <m.kazinauskas@gmail.com>
2021-02-27 19:33:51 +0200

    Pagination in separate component

--- src/components/search/radio-stations-results.tsx
+++ src/components/search/radio-stations-results.tsx
@@ -1,5 +1,6 @@
 import NoSearchResultsComponent from "./no-search-results-component";
 import { Response, Data } from '../../api/search/radio-stations-search';
+import PaginationComponent from "./pagination-component";
 
 function RadioStationsResults(params: { searchResults: Response}) {
     const { searchResults } = params;
@@ -19,54 +20,10 @@ function RadioStationsResults(params: { searchResults: Response}) {
         <section className="bg-gray-50">
             <h1 className="text-2xl text-secondary font-bold flex-1 px-10 md:pt-16 py-5 text-center">Radio stations with name: "{searchResults.query}"</h1>
             {radioStationResults}
-            {getPagination(searchResults.query, searchResults.number, searchResults.totalPages)}
+            <PaginationComponent baseUrl='/search/by-radio-station' query={searchResults.query} currentPage={searchResults.number} lastPage={searchResults.totalPages} />
         </section>
     )
 
-}
-
-function getPagination(searchTitle: string, number: number, totalPages: number) {
-    console.log(number);
-    console.log(totalPages);
-    const safeSearchTitle = searchTitle.replaceAll(' ', '-').toLocaleLowerCase();
-
-    const baseLink = `/search/by-radio-station/${safeSearchTitle}/`;
-
-    let buttons = [];
-
-    if (number <= 0) {
-        buttons.push((
-            <a key="prev-disabled" className="mx-1 px-3 py-2 bg-white text-gray-500 font-medium rounded-md cursor-not-allowed">
-                Previous
-            </a>
-        ));
-    } else {
-        buttons.push((
-            <a key="prev" href={baseLink + '?number=' + (number - 1)} className="mx-1 px-3 py-2 bg-white text-gray-700 hover:bg-blue-500 hover:text-white font-medium rounded-md">
-                Previous
-            </a>
-        ));
-    }
-
-    if (number >= totalPages) {
-        buttons.push((
-            <a key="next-disabled" className="mx-1 px-3 py-2 bg-white text-gray-500 font-medium rounded-md cursor-not-allowed">
-                Next
-            </a>
-        ));
-    } else {
-        buttons.push((
-            <a key="next" href={baseLink + '?number=' + (number + 1)} className="mx-1 px-3 py-2 bg-white text-gray-700 hover:bg-blue-500 hover:text-white font-medium rounded-md">
-                Next
-            </a>
-        ));
-    }
-
-    return (
-        <div className="pb-12 pt-6 mx-auto text-center">
-            {buttons}
-        </div>
-    )
 }
 
 function stationResult(item?: Data) {
