07da84baef9720420e50f6547b00ebbb16c7b06c
Modestas <m.kazinauskas@gmail.com>
2021-03-02 09:35:00 +0200

    Margin to padding update

--- src/components/radio-station/radio-station-info-panel.tsx
+++ src/components/radio-station/radio-station-info-panel.tsx
@@ -104,7 +104,7 @@ function _streams(streams: RadioStationStream[]) {
 function _songs(songs: RadioStationSong[]) {
     console.log(songs);
     const songsTable = songs.map(song => (
-        <li className="flex items-center space-x-3 my-2 pl-3 hover:bg-gray-100" key={song.id}>
+        <li className="flex items-center space-x-3 py-2 pl-3 hover:bg-gray-100" key={song.id}>
             <div className="flex-1 break-all">{song.title}</div>
             <div className="flex-1">{new Date(song.playedTime).toLocaleString()}</div>
         </li>
@@ -114,7 +114,7 @@ function _songs(songs: RadioStationSong[]) {
     return (
         <div className="col-span-5 px-5">
             <h2 className="text-gray-800 capitalize text-xl font-bold mb-5 text-center pt-10">Played songs</h2>
-            <li className="items-center space-x-3 mb-3 pl-3 hidden md:flex" key="info">
+            <li className="items-center space-x-3 pb-3 pl-3 hidden md:flex" key="info">
                 <div className="flex-1 font-bold">Title</div>
                 <div className="flex-1 font-bold">Played Time</div>
             </li>
