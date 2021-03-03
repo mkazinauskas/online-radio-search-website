9e96ef061384735ba6144d2875f1dd6ce75def22
Modestas <m.kazinauskas@gmail.com>
2021-03-03 09:04:48 +0200

    Last 50 searches update

--- src/components/last-searches-component.tsx
+++ src/components/last-searches-component.tsx
@@ -2,20 +2,19 @@ import ApiResponseHolder from "../api/api-response-holder";
 import { LastSearch, LastSearchesResponse } from "../api/last-searches/last-searhes";
 import { toSeoText } from "../utils/seo-tools";
 
-interface TypeToStringMap {
-    [name: string]: string
+interface ConfigType {
+    textTitle: string,
+    linkPrefix: string
 }
 
-const typeToText: TypeToStringMap = {
-    'song': 'song',
-    'radiostation': 'radio station',
-    'genre': 'genre'
+interface TypeToConfigMapType {
+    [name: string]: ConfigType
 }
 
-const typeToUrl: TypeToStringMap = {
-    'song': '/search/by-song/',
-    'radiostation': '/search/by-radio-station/',
-    'genre': '/search/by-genre/'
+const configFromType: TypeToConfigMapType = {
+    'song': { textTitle: 'song', linkPrefix: '/search/by-song/' },
+    'radiostation': { textTitle: 'radio station', linkPrefix: '/search/by-radio-station/' },
+    'genre': { textTitle: 'genre', linkPrefix: '/search/by-genre/'}
 }
 
 function LastSearchesComponent(params: { lastSearhesResponseHolder: ApiResponseHolder<LastSearchesResponse> }) {
@@ -29,12 +28,14 @@ function LastSearchesComponent(params: { lastSearhesResponseHolder: ApiResponseH
         return '';
     }
 
-    const { searches } = lastSearhesResponseHolder?.response;
+    const { searches } = lastSearhesResponseHolder.response;
 
-    const searchLinks = searches.map((search: LastSearch) => createLink(search)).reduce((prev, curr) => [prev, ' - ', curr]);
+    const searchLinks = searches
+        .map((search: LastSearch) => createLink(search))
+        .reduce((prev: any, curr: any) => [prev, ' - ', curr]);
 
     return (
-        <div className="max-w-screen-xl mx-auto mt-10">
+        <div className="container mx-auto mt-1 md:mt-10">
             <div className="text-center relative px-4 py-6 overflow-hidden sm:px-6 sm:py-8 lg:p-12 xl:p-16">
                 <h2 className="text-xl font-semibold font-display text-gray-900 sm:text-3xl">Last {searches.length} searches</h2>
                 <p className="mt-4 text-base text-gray-500">
@@ -47,8 +48,9 @@ function LastSearchesComponent(params: { lastSearhesResponseHolder: ApiResponseH
 }
 
 function createLink(search: LastSearch) {
-    const linkText = `${search.query} ${typeToText[search.type]}`;
-    const link = `${typeToUrl[search.type]}${toSeoText(search.query)}`;
+    const data = configFromType[search.type]
+    const linkText = `${search.query} ${data.textTitle}`;
+    const link = `${data.linkPrefix}${toSeoText(search.query)}`;
     return (<a key={search.id} href={link} className="hover:text-gray-900 no-underline hover:underline">{linkText}</a>)
 }
 
